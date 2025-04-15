04ae288203d0415a385ce3453b437f9ed35dd22a
TylerZhou <zypqqgc@qq.com>
2025-04-16 07:47:30 +0800

    feat 增加获取发起人自选节点方法

--- antflowcore/controller/BpmBusinessController.cs
+++ antflowcore/controller/BpmBusinessController.cs
@@ -1,8 +1,11 @@
-﻿using antflowcore.dto;
+﻿using antflowcore.constant.enus;
+using antflowcore.dto;
 using AntFlowCore.Entity;
+using antflowcore.exception;
 using antflowcore.service.biz;
 using antflowcore.service.repository;
 using antflowcore.util;
+using antflowcore.vo;
 using AntFlowCore.Vo;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,12 +16,15 @@ public class BpmBusinessController
 {
     private readonly TaskMgmtService _taskMgmtService;
     private readonly UserEntrustService _userEntrustService;
+    private readonly BpmnNodeService _bpmnNodeService;
 
     BpmBusinessController(TaskMgmtService taskMgmtService,
-        UserEntrustService userEntrustService)
+        UserEntrustService userEntrustService,
+        BpmnNodeService bpmnNodeService)
     {
         _taskMgmtService = taskMgmtService;
         _userEntrustService = userEntrustService;
+        _bpmnNodeService = bpmnNodeService;
     }
     [HttpGet("GetDIYFormCodeList")]
     public Result<List<DIYProcessInfoDTO>> GetDIYFormCodeList(String desc) {
@@ -43,5 +49,26 @@ public class BpmBusinessController
     {
         _userEntrustService.UpdateEntrustList(dataVo);
         return ResultHelper.Success("ok");
+    }
+    [HttpGet("getStartUserChooseModules")]
+    public Result<List<BpmnNodeVo>> GetStartUserChooseModules([FromQuery] string formCode)
+    {
+        if (string.IsNullOrWhiteSpace(formCode))
+        {
+            throw new AFBizException("参数formCode不能为空!");
+        }
+
+        
+        List<BpmnNode> nodes = _bpmnNodeService.GetNodesByFormCodeAndProperty(
+            formCode, (int)NodePropertyEnum.NODE_PROPERTY_CUSTOMIZE
+        );
+
+        var nodeVos = nodes.Select(a => new BpmnNodeVo
+        {
+            Id = a.Id,
+            NodeName = a.NodeName
+        }).ToList();
+
+        return ResultHelper.Success(nodeVos);
     }
 }
--- antflowcore/service/repository/BpmnNodeService.cs
+++ antflowcore/service/repository/BpmnNodeService.cs
@@ -1,4 +1,5 @@
-﻿using AntFlowCore.Entity;
+﻿using antflowcore.entity;
+using AntFlowCore.Entity;
 
 namespace antflowcore.service.repository;
 
@@ -6,5 +7,15 @@ public class BpmnNodeService : AFBaseCurdRepositoryService<BpmnNode>
 {
     public BpmnNodeService(IFreeSql freeSql) : base(freeSql)
     {
+    }
+
+    public List<BpmnNode> GetNodesByFormCodeAndProperty(string formCode, int property)
+    {
+        List<BpmnNode> bpmnNodes = this.Frsql.Select<BpmnConf, BpmnNode>()
+            .InnerJoin((a, b) => a.Id == b.ConfId)
+            .Where((a, b) => a.FormCode == formCode && a.EffectiveStatus == 1 && b.NodeProperty == property)
+            .OrderByDescending((a, b) => a.CreateTime)
+            .ToList((a,b)=>b);
+        return bpmnNodes;
     }
 }
