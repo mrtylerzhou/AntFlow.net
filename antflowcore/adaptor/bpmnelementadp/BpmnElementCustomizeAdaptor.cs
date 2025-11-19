6f0625279ab5c62bb0f8a49e497d3c57ff3d9bb8
TylerZhou <zypqqgc@qq.com>
2025-11-19 19:40:44 +0800

    修复自选审批人不生效的bug

--- antflowcore/adaptor/bpmnelementadp/BpmnElementCustomizeAdaptor.cs
+++ antflowcore/adaptor/bpmnelementadp/BpmnElementCustomizeAdaptor.cs
@@ -74,49 +74,7 @@ using Microsoft.Extensions.Logging;
             }
         }
 
-        public override void DoFormatNodesToElements(List<BpmnConfCommonElementVo> bpmnConfCommonElementVos, BpmnNodeVo nodeVo, int nodeCode, int sequenceFlowNum, Dictionary<string, int> numMap)
-        {
-            /*BpmnNodeParamsVo paramsVo = nodeVo.Params;
-            BpmnNodePropertysVo property = nodeVo.Property??new BpmnNodePropertysVo();
-            int signType = property.SignType ?? 1;
-
-            // Filter already deduplicated assignees
-            List<BpmnNodeParamsAssigneeVo> paramsAssigneeVos = paramsVo.AssigneeList
-                .Where(o => o.IsDeduplication == 0)
-                .ToList();
-
-            foreach (var bpmnNodeParamsAssigneeVo in paramsAssigneeVos)
-            {
-                int elementCodeIncremented = nodeCode + 1;
-                string elementId = ProcessNodeEnum.GetDescByCode(elementCodeIncremented);
-                int elementSequenceFlowNum = sequenceFlowNum + 1;
-
-                var singleAssigneeMap = new Dictionary<string, string>
-                {
-                    { bpmnNodeParamsAssigneeVo.Assignee, bpmnNodeParamsAssigneeVo.AssigneeName }
-                };
-
-                BpmnConfCommonElementVo elementVo = BpmnElementUtils.GetSingleElement(elementId, bpmnNodeParamsAssigneeVo.ElementName,
-                    $"customizeUser{elementCodeIncremented}", bpmnNodeParamsAssigneeVo.Assignee, singleAssigneeMap);
-
-                // Set node id and buttons
-                elementVo.NodeId = nodeVo.Id.ToString();
-                elementVo.SignType = signType;
-                base.SetElementButtons(nodeVo, elementVo);
-                bpmnConfCommonElementVos.Add(elementVo);
-
-                BpmnConfCommonElementVo sequenceFlowElement = BpmnElementUtils.GetSequenceFlow(elementSequenceFlowNum,
-                    ProcessNodeEnum.GetDescByCode(nodeCode), elementVo.ElementId);
-
-                bpmnConfCommonElementVos.Add(sequenceFlowElement);
-
-                nodeCode++;
-                sequenceFlowNum++;
-
-                numMap["nodeCode"] = nodeCode;
-                numMap["sequenceFlowNum"] = sequenceFlowNum;
-            }*/
-        }
+       
 
         public override void SetSupportBusinessObjects()
         {
